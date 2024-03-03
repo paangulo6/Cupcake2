3f75fb70fc32a22974c1aa41000e2d80448119ec
paangulo6 <paulalardero581@gmail.com>
2024-03-03 18:56:49 +0100

    Paso4

--- app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
+++ app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
@@ -1,2 +1,24 @@
 package com.example.cupcake.test
 
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import org.junit.Rule
+import androidx.navigation.testing.TestNavHostController
+import com.example.cupcake.CupcakeApp
+import org.junit.Before
+
+private lateinit var navController: TestNavHostController
+
+@get:Rule
+val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+@Before
+fun setupCupcakeNavHost() {
+    composeTestRule.setContent {
+        navController = TestNavHostController(LocalContext.current).apply {
+            navigatorProvider.addNavigator(ComposeNavigator())
+        }
+        CupcakeApp(navController = navController)
+    }
+}
